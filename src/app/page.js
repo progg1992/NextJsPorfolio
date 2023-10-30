1310c06d61bf634003f0650a883413ce920002f4
progg1992 <pierce.rogg@mymail.nwtc.edu>
2023-10-29 23:35:34 -0500

    Feat(Arch) NavBar Implementation

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import AboutSection from '@/components/AboutSection';
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col bg-[#121212]'>
+			<Navbar />
 			<div className='container mx-auto mt-24 px-12 py-4'>
 				<HeroSection />
 			</div>
